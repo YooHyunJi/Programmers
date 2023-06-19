81ebceea5f04df44700785f9499c9a202940bfc1
YooHyunJi <ccamy0915@naver.com>
2023-06-19 16:50:54 +0900

    230619

---
+++ Lv.2/피로도/피로도.py
@@ -0,0 +1,42 @@
+import itertools
+
+
+def solution(k, dungeons):
+    answer = 0
+    len_dungeons = len(dungeons)
+
+    # 1
+    sequences = list(itertools.permutations([i for i in range(0, len_dungeons)], len_dungeons))
+
+    # 2
+    for i in range(len(sequences)):
+        hp = k
+        possible_dungeons = 0
+
+        # 3
+        for j in range(len_dungeons):
+            this_dungeon = dungeons[sequences[i][j]]
+
+            # 4
+            if hp >= this_dungeon[0]:
+                possible_dungeons += 1
+                hp -= this_dungeon[1]
+            else:
+                break
+
+        # 5
+        if possible_dungeons > answer:
+            answer = possible_dungeons
+            # 6
+            if answer == len_dungeons:
+                break
+
+    print(answer)
+    return answer
+
+
+if __name__ == '__main__':
+    k = 80
+    dungeons = [[80, 20], [50, 40], [30, 10]]
+
+    solution(k, dungeons)
